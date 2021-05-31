31ebd982d755b9ff4ce96ebe2a88cffb03b6bfc7
sschacon <sschacon@gmail.com>
2021-05-31 21:10:34 +0000

    First Commit to Solucion_Proyecto_Fiori

--- HTML5Project/webapp/controller/CreateEmployee.controller.js
+++ HTML5Project/webapp/controller/CreateEmployee.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
 ],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, History) {
         "use strict";
 
         return Controller.extend("logaligroup.HTML5Project.controller.CreateEmployee", {
@@ -12,6 +13,18 @@ sap.ui.define([
 
             },
 
+            onBack: function (oEvent) {
+
+                var oHistory = History.getInstance();
+                var sPreviousHash = oHistory.getPreviousHash();
+
+                if (sPreviousHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                    oRouter.navTo("menu", true);
+                }
+            },
 
 
         });
